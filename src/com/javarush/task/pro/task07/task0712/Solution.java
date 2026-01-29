ede75086fabe13ce7cbdeafc21cd6bdf4df06922
juryt <utroshkin@mail.ru>
2026-01-29 20:52:27 +0300

    Start project

---
+++ src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String cat = new String("Кот");
+        String name = new String("Vasya");
+        String men = new String("Человек");
+        String home = new String("Дом");
+        String wife = new String("Жена");
+        String husband = new String("Муж");
+        String daughter = new String("Дочка");
+        String car = new String("Феррари");
+        String work = new String("Программист");
+        String salary = new String("Зарплата");
+        int[]presents = new int[10];
+        int[]wish = new int[5];
+        int[]money = new int[2];
+        int[]animals = new int[1];
+        int[]cars = new int[0];
+        Scanner scanner = new Scanner(car);
+        Scanner secondScanner = new Scanner(work);
+
+
+    }
+}
