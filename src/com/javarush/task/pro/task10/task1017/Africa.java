456d948ff6a7f9e8746e2a9e601a2504d150de4f
juryt <utroshkin@mail.ru>
2026-02-02 11:36:56 +0300

    Start project

---
+++ src/com/javarush/task/pro/task10/task1017/Africa.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task10.task1017;
+
+public class Africa {
+    private final int area;
+
+    public Africa(int area) {
+        this.area = area;
+    }
+
+    public int getAREA() {
+        return area;
+    }
+}
+
---
+++ src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Africa africa = new Africa(500);
+        Antarctica antarctica = new Antarctica(600);
+        Australia australia = new Australia(700);
+        Eurasia eurasia = new Eurasia(800);
+        NorthAmerica northAmerica = new NorthAmerica(900);
+        SouthAmerica southAmerica = new SouthAmerica(1000);
+    }
+}
