30312c2fefd9b8d5cfbae3d59a691ef30c50c27f
juryt <utroshkin@mail.ru>
2026-02-18 18:37:25 +0300

    Start project

---
+++ src/com/javarush/task/jdk13/task53/task5302/GameObject.java
@@ -0,0 +1,13 @@
+package com.javarush.task.jdk13.task53.task5302;
+
+public class GameObject {
+    public int x;
+    public int y;
+    public boolean isMine;
+    public GameObject(int x, int y, boolean isMine) {  //  конструктор
+        this.x = x;
+        this.y = y;
+        this.isMine = isMine;
+    }
+}
+
--- src/com/javarush/task/jdk13/task53/task5302/MinesweeperGame.java
+++ src/com/javarush/task/jdk13/task53/task5302/MinesweeperGame.java
@@ -4,8 +4,27 @@ import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;  //  переменная константа задающаая размер поля
+    private GameObject gameField[][] = new GameObject [SIDE][SIDE];  //  масиив ячеек
+    private int countMinesOnField; //  счетчик мин
+
+    private void createGame () {  //  создали метод
+        for (int x = 0; x < SIDE; x++) {
+            for (int y = 0; y < SIDE; y++) { //  прошлись циклом по массиву
+               int randomNumber = getRandomNumber(10); //  генерируем рандом
+                boolean isMine = randomNumber < 1;  //  вероятность 10%
+                if (isMine){
+                    countMinesOnField++;  //  счетчик мин
+                }
+                gameField [y][x] = new GameObject(x,y, isMine);  //  присвоили значения ячейкам
+                setCellColor(x,y, Color.ORANGE);  //  закрасили ячейки
+            }
+        }
+    }
+
 
     public void initialize(){
         setScreenSize(SIDE,SIDE);  // задаем размер игрового поля
+        createGame();
     }
+
 }
