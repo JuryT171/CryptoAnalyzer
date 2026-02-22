9854d5437fa42fe3a28a490cbb3c5c5a904bc99a
juryt <utroshkin@mail.ru>
2026-02-22 19:23:31 +0300

    Start project

---
+++ src/com/javarush/task/pro/task13/task1330/MySingleton.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task13.task1330;
+
+public enum MySingleton {
+    INSTANCE;
+    private MySingleton(){
+        System.out.println("создание экземпляра...");
+    }
+}
