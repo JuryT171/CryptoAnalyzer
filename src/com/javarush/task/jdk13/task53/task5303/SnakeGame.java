f6a240bb2ec3862e9825b1096c2a8488d9265565
juryt <utroshkin@mail.ru>
2026-03-01 20:16:58 +0300

    Start project

--- src/com/javarush/task/jdk13/task53/task5303/SnakeGame.java
+++ src/com/javarush/task/jdk13/task53/task5303/SnakeGame.java
@@ -3,11 +3,25 @@ package com.javarush.task.jdk13.task53.task5303;
 import com.javarush.engine.cell.*;
 
 public class SnakeGame extends Game {
-    public static final int HEIGHT = 15;
+    public static final int HEIGHT = 15;  //  константы размера поля
     public static final int WIDTH = 15;
 
     @Override
-    public void initialize() {
+    public void initialize() {    // переопределили метод
         setScreenSize(WIDTH,HEIGHT);
+        createGame();
+    }
+
+    private void createGame(){
+        drawScene();  // вызвали метод для открашивания игровой площадки
+    }
+
+    private void drawScene(){
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                setCellColor(x, y, Color.BLUE);  //  закрасили поле с помощью цикла и метода setCellColor
+            }
+        }
     }
 }
+
