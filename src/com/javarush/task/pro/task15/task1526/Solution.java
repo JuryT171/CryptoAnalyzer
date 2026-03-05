32bb8cf42b1d1b85f7afc2a2f5ccc25d4725683e
juryt <utroshkin@mail.ru>
2026-03-05 11:21:24 +0300

    Start project

--- src/com/javarush/task/pro/task15/task1526/Solution.java
+++ src/com/javarush/task/pro/task15/task1526/Solution.java
@@ -10,5 +10,23 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);  //создаем сканнер
+        String path = scanner.nextLine();  //  записываем путь к файлу
+        try {
+            FileReader fileReader = new FileReader(path);  //  создаем ридер
+            int character;  //  вводим переменную
+            while ((character = fileReader.read()) != -1) { // Читаем по одному символу до конца файла
+                char c = (char) character;  //преобразуем инт в чар
+                if (c != '.' && c != ',' && c != ' ') { // Проверяем на символы
+                    System.out.print(c);
+                }
+            }
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        scanner.close();
     }
 }
